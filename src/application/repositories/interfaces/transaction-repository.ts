957c1b95cccd8485e90be6524718e2fab1180633
Lucas Farias <lucasfarias.ln@gmail.com>
2024-12-26 19:38:41 -0300

    feat: create update transaction repository

--- src/application/repositories/interfaces/transaction-repository.ts
+++ src/application/repositories/interfaces/transaction-repository.ts
@@ -4,4 +4,9 @@ export interface TransactionsRepository {
   create(
     createTransactionParams: Prisma.TransactionsCreateInput,
   ): Promise<Transactions>
+
+  update(
+    transactionId: string,
+    updateTransactionParams: Prisma.TransactionsUpdateInput,
+  ): Promise<Transactions | null>
 }

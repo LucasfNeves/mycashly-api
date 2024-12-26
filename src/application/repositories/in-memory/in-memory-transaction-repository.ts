b75cea2ef22fa04565fbea165e209b555623f51f
Lucas Farias <lucasfarias.ln@gmail.com>
2024-12-26 19:32:51 -0300

    feat: add in-memory-transaction-repository

--- src/application/repositories/in-memory/in-memory-transaction-repository.ts
+++ src/application/repositories/in-memory/in-memory-transaction-repository.ts
@@ -1,4 +1,4 @@
-import { Prisma, Transactions } from '@prisma/client'
+import { Prisma, Transactions, TransactionType } from '@prisma/client'
 import { TransactionsRepository } from '../interfaces/transaction-repository'
 import { randomUUID } from 'crypto'
 
@@ -24,5 +24,46 @@ export class inMemoryTransactionsRepository implements TransactionsRepository {
     this.transactions.push(transaction)
 
     return transaction
+  }
+
+  async update(
+    transactionId: string,
+    updateTransactionParams: Prisma.TransactionsUpdateInput,
+  ): Promise<Transactions | null> {
+    const transaction = this.transactions.find(
+      (transaction) => transaction.id === transactionId,
+    )
+
+    if (!transaction) {
+      return null
+    }
+
+    // Helper para pegar o valor ou manter o original
+    const getValue = <T>(newValue: T | undefined, originalValue: T) =>
+      newValue !== undefined ? newValue : originalValue
+
+    const updatedTransaction = {
+      ...transaction,
+      name: getValue(updateTransactionParams.name as string, transaction.name),
+      value: getValue(
+        updateTransactionParams.value as number,
+        transaction.value,
+      ),
+      type: getValue(
+        updateTransactionParams.type as TransactionType,
+        transaction.type,
+      ),
+      date: getValue(updateTransactionParams.date as Date, transaction.date),
+      categoryId: getValue(
+        updateTransactionParams.category?.connect?.id,
+        transaction.categoryId,
+      ),
+    }
+
+    this.transactions = this.transactions.map((transaction) =>
+      transaction.id === transactionId ? updatedTransaction : transaction,
+    )
+
+    return updatedTransaction
   }
 }

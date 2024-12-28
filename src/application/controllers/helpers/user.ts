a100a5c2723a7f1cc105f5e2555e5afb12807f90
Lucas Farias <lucasfarias.ln@gmail.com>
2024-12-28 10:37:48 -0300

    refactor: add JsDocs for utilitaries functions

--- src/application/controllers/helpers/user.ts
+++ src/application/controllers/helpers/user.ts
@@ -1,5 +1,17 @@
 import { userNotFound } from './http'
 
+/**
+ * Generate a response for a user not found
+ * @returns An object with a message indicating that the user was not found
+ * @throws Return a user not found response with a message indicating that the user was not found
+ * @throws Return error response with status code 404
+ * @example
+ * const response = userNotFoundResponse()
+ * Returns: {
+ *   errorMessage: 'User not found'
+ * }
+ */
+
 export const userNotFoundResponse = () => {
   return userNotFound({ errorMessage: 'User not found' })
 }

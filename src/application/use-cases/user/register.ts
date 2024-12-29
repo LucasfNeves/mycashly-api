3dfa24a2acd85e4c764a112eb24696dd709845c1
Lucas Farias <lucasfarias.ln@gmail.com>
2024-12-28 23:18:59 -0300

    refactor: register use-case add refresh token

--- src/application/use-cases/user/register.ts
+++ src/application/use-cases/user/register.ts
@@ -1,8 +1,14 @@
-import { sign } from 'jsonwebtoken'
 import { UserAlreadyExists } from '../../../errors/user-already-exists'
 import { UsersRepository } from '../../repositories/interfaces/users-repository'
 import bcrypt from 'bcryptjs'
-import { env } from '../../../config/env'
+import { PrismaRefreshTokenRepository } from '../../repositories/postgres/prisma-refresh-token-repository'
+import {
+  ACCESS_TOKEN_EXPIRATION,
+  BCRYPT_SALT_ROUNDS,
+  EXP_TIME_IN_DAYS,
+} from '../../../config/constants'
+import { calculateExpirationDate } from '../../../utils/calculate-expiration-date'
+import { JwtAdapter } from '../../../adapters/jwt-adapter'
 interface RegisterUserUseCaseParams {
   email: string
   name: string
@@ -10,18 +16,39 @@ interface RegisterUserUseCaseParams {
 }
 
 interface RegisterUseCaseResponse {
-  acessToken: string
+  accessToken: string
+  refreshTokenId: string
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private readonly refreshTokenRepository: PrismaRefreshTokenRepository,
+    private readonly jwtAdapter: JwtAdapter,
+  ) {}
+
+  private async generateTokens(
+    userId: string,
+  ): Promise<{ accessToken: string; refreshTokenId: string }> {
+    const accessToken = this.jwtAdapter.sign(
+      { sub: userId },
+      ACCESS_TOKEN_EXPIRATION,
+    )
+    const expiresAt = calculateExpirationDate(EXP_TIME_IN_DAYS)
+    const { id: refreshTokenId } = await this.refreshTokenRepository.create(
+      userId,
+      expiresAt,
+    )
+
+    return { accessToken, refreshTokenId }
+  }
 
   async execute(
     params: RegisterUserUseCaseParams,
   ): Promise<RegisterUseCaseResponse> {
     const { name, email, password } = params
 
-    const hasedPassword = await bcrypt.hash(password, 12)
+    const hasedPassword = await bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
@@ -35,12 +62,10 @@ export class RegisterUseCase {
       password: hasedPassword,
     })
 
-    const acessToken = sign({ sub: createdUser.id }, env.jwtSecret!, {
-      expiresIn: '7d',
-    })
+    const { accessToken, refreshTokenId } = await this.generateTokens(
+      createdUser.id,
+    )
 
-    return {
-      acessToken,
-    }
+    return { accessToken, refreshTokenId }
   }
 }

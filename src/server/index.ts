19363084d9e034e36eef89243cf6442bc1797041
Lucas Farias <lucasfarias.ln@gmail.com>
2025-01-07 12:34:04 -0300

    feat: add swagger json

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import { router } from './routes/routes'
+import swaggerUi from 'swagger-ui-express'
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup())
 
 const port = process.env.PORT
 app.listen(port, () => {

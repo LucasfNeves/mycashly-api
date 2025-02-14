5acafe79cf038a69bf89870483b21b88d7e44260
Lucas Farias <lucasfarias.ln@gmail.com>
2025-02-14 11:46:15 -0300

    feat: add get-top-five-expenses tests

--- src/tests/fixtures/transaction.ts
+++ src/tests/fixtures/transaction.ts
@@ -11,6 +11,16 @@ export const transaction = {
   value: Number(faker.finance.amount()),
 }
 
+export const expenseTransaction = {
+  id: '1',
+  userId: faker.string.uuid(),
+  categoryId: '3',
+  name: faker.commerce.productName(),
+  date: faker.date.anytime().toISOString(),
+  type: 'EXPENSE' as TransactionType,
+  value: Number(faker.finance.amount()),
+}
+
 export const userBalance = {
   earnings: faker.finance.amount(),
   expenses: faker.finance.amount(),
